dd03d82703b88ba6e6adb2621e1cbe6f640de5c5
vallas01 <vallasac@gmail.com>
2022-08-02 09:53:08 -0400

    404 not found

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
+import NotFound from './components/NotFound';
 import { authenticate } from './store/session';
 
 function App() {
@@ -43,6 +44,11 @@ function App() {
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute>
+
+        <Route path=''>
+          <NotFound />
+        </Route>
+
       </Switch>
     </BrowserRouter>
   );
---
+++ react-app/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="notfound">
+            <div>Oops!</div>
+            <div>Something went wrong...404</div>
+
+        </div>
+    )
+}
+
+export default NotFound
