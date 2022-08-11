73e219416a23e9d15700a2cfabe69e4985d3c4a2
vallas01 <vallasac@gmail.com>
2022-08-11 08:16:02 -0400

    reset added to logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
+  // eslint-disable-next-line
+  const [username, setUsername] = useState('');
+  // eslint-disable-next-line
+  const [first_name, setFirstname] = useState('');
+  // eslint-disable-next-line
+  const [last_name, setLastname] = useState('');
+  // eslint-disable-next-line
+  const [email, setEmail] = useState('');
   const history = useHistory()
   const dispatch = useDispatch()
+
   const onLogout = async (e) => {
     await dispatch(logout());
+    setUsername('')
+    setFirstname('')
+    setLastname('')
+    setEmail('')
     history.push('/')
   };
 
